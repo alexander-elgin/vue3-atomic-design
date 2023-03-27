4d5b57617ee023975a8ca1d0b17747dd6189c2dd
Alexander Elgin <sascha.elgin@gmail.com>
2023-03-27 12:00:44 +0600

    extract pinia plugin

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import i18n from './plugins/i18n'
+import pinia from './plugins/pinia'
 import router from './plugins/router'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(i18n)
 app.use(router)
 
