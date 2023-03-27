3ac23c61e4900c49e12815ddb21debe852b7c920
Alexander Elgin <sascha.elgin@gmail.com>
2023-03-27 16:45:49 +0600

    integrate vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import i18n from './plugins/i18n'
 import pinia from './plugins/pinia'
 import router from './plugins/router'
+import vuetify from './plugins/vuetify'
 
 import './assets/main.css'
 
@@ -12,5 +13,6 @@ const app = createApp(App)
 app.use(pinia)
 app.use(i18n)
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
