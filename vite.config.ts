2df0229c326f46968b741d900f5ecbc45ae0cc2a
Alexander Elgin <sascha.elgin@gmail.com>
2023-03-29 11:07:22 +0600

    integrate postcss nesting

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import postcssNesting from 'postcss-nesting';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        postcssNesting
+      ],
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
