46861ea80cf1ab9224f93d2e15ed055f155dee5e
Alexander Elgin <sascha.elgin@gmail.com>
2023-03-30 20:45:12 +0600

    set up fontawesome icons

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,9 +1,16 @@
+import '@fortawesome/fontawesome-free/css/all.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa'
 
 export default createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: { fa }
+  },
 })
